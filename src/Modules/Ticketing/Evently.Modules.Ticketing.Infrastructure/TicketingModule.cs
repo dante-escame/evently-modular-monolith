73fc50745162bcd3dfc50cf1530548d162ed756d
Dante Escame <dante.escame@gmail.com>
2025-08-21 21:45:13 -0300

    feat: creating "IntegrationEvents", sync communication and async communication

--- src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -1,4 +1,14 @@
-﻿using Evently.Common.Presentation.Endpoints;
+﻿using Evently.Common.Infrastructure.Outbox;
+using Evently.Common.Presentation.Endpoints;
+using Evently.Modules.Ticketing.Application.Abstractions.Data;
+using Evently.Modules.Ticketing.Application.Carts;
+using Evently.Modules.Ticketing.Domain.Customers;
+using Evently.Modules.Ticketing.Infrastructure.Customers;
+using Evently.Modules.Ticketing.Infrastructure.Database;
+using Evently.Modules.Ticketing.Presentation.Customers;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,8 +27,26 @@ public static class TicketingModule
         return services;
     }
 
+    public static void ConfigureConsumers(IRegistrationConfigurator registrationConfigurator)
+    {
+        registrationConfigurator.AddConsumer<UserRegisteredIntegrationEventConsumer>();
+    }
+
     private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        // Will implement this later.
+        services.AddDbContext<TicketingDbContext>((sp, options) =>
+            options
+                .UseNpgsql(
+                    configuration.GetConnectionString("Database"),
+                    npgsqlOptions => npgsqlOptions
+                        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Ticketing))
+                .AddInterceptors(sp.GetRequiredService<PublishDomainEventsInterceptor>())
+                .UseSnakeCaseNamingConvention());
+
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<TicketingDbContext>());
+
+        services.AddSingleton<CartService>();
     }
 }
